7354cfe647ca79a50732f2c309b1dc6b540f6d4e
Qadir <qadirb3492@gmail.com>
2026-06-23 16:41:45 +0500

    Added a program to swap two numbers using a third variable

---
+++ Fundamentals/Variables, Datatypes, and Operators/SwappingTwoNumbers1.kt
@@ -0,0 +1,16 @@
+fun main(){
+	var num1 : Int = 12
+	var num2 : Int = 19
+	
+	println("Before swapping: ")
+	println("Num1 = "+num1)
+	println("Num2 = "+num2)
+	
+	var temp : Int = num1
+	num1 = num2
+	num2 = temp
+	
+	println("After swapping: ")
+	println("Num1 = "+num1)
+	println("Num2 = "+num2)
+}
