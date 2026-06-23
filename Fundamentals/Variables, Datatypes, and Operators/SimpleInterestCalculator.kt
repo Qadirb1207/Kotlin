1a17e4a6afb1245f0b2f552d3b82342a765bb30e
Qadir <qadirb3492@gmail.com>
2026-06-23 16:35:39 +0500

    Implemented a program to find the simple interest

---
+++ Fundamentals/Variables, Datatypes, and Operators/SimpleInterestCalculator.kt
@@ -0,0 +1,10 @@
+fun main(){
+	var time: Int = 3
+	var rate : Int = 5
+	var principal : Int = 12000
+	
+	var simpleInterest: Int = time*rate*principal
+	
+	println("Simple Interest = "+simpleInterest)
+	
+}
