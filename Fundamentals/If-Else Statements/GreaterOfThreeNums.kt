df91a93c3323972f7cec5df98567ad2653b85f9b
Qadir <qadirb3492@gmail.com>
2026-06-24 19:14:12 +0500

    Implemented a program to find the greatest of three integers

---
+++ Fundamentals/If-Else Statements/GreaterOfThreeNums.kt
@@ -0,0 +1,13 @@
+fun main(){
+	var num1: Int = 19
+	var num2 : Int = 29
+	var num3 : Int =28
+	
+	if(num1 > num2 && num1 > num3){
+		println("Greatest of all is: "+num1)
+	}else if(num2 > num3){
+		println("Greatest of all is: "+num2)
+	}else{
+		println("Greatest of all is: "+num3)
+	}
+}
