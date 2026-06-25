863d8189cdd59da00a14c3c459da6ecda4e79200
Qadir <qadirb3492@gmail.com>
2026-06-25 17:38:37 +0500

    Added a program to check a person can vote or not
    Implemented a program to check whether a person can vote or not

---
+++ Fundamentals/If-Else Statements/VoteEligibility.kt
@@ -0,0 +1,11 @@
+fun main(){
+	var age : Int = 19
+	
+	if(age < 1){
+		println("Invalid Age!!")
+	}else if(age >= 18){
+		println("You're eligible to vote!!")
+	}else{
+		println("You're not eligible to vote!!")
+	}
+}
