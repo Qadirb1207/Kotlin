047339f77650ee3121fa0a04527ac5d3e2aa7c03
Qadir <qadirb3492@gmail.com>
2026-06-25 17:35:49 +0500

    Implemented a program to check a number
    Implemented a program to check a number is positive, negative or zero

---
+++ Fundamentals/If-Else Statements/NumberChecking.kt
@@ -0,0 +1,11 @@
+fun main(){
+	var num : Int = 0
+	
+	if(num > 0){
+		println("Number is Positive")
+	}else if(num < 0){
+		println("Number is Negative")
+	}else{
+		println("Number is Zero")
+	}
+}
