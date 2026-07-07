9ebdc55acf5eb99ffd6aa663dfbd15cc02938cf4
Qadir <qadirb3492@gmail.com>
2026-07-07 17:14:10 +0500

    Implemented a program to print even numbers

---
+++ Fundamentals/Loops/PrintEvenNumbers.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main(){
+	val sc = Scanner(System.`in`)
+	print("Enter a number = ")
+	var num = sc.nextInt()
+	
+	for(i in 2..num step 2){
+		println(i)
+	}
+	
+	sc.close()
+}
