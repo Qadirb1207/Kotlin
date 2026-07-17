f37e5ff27d15c652d0ec759177226b3235c9e60c
Qadir <qadirb3492@gmail.com>
2026-07-17 17:11:26 +0500

    Added a program to create a circle class

---
+++ Object Oriented Programming/Classes and Objects/Circle.kt
@@ -0,0 +1,6 @@
+class Circle(var radius: Float){
+	
+	fun area(): Double{
+		return Math.PI*radius*radius;
+	}
+}
---
+++ Object Oriented Programming/Classes and Objects/CircleDemo.kt
@@ -0,0 +1,4 @@
+fun main(){
+	val circle = Circle(21.0f);
+	println("Area of the circle with radius ${circle.radius} units is: ${circle.area()} square units");
+}
